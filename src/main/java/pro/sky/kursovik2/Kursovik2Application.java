6cd0ea6c9ef939aad79ef34486cbc1d241d3de9a
Alex1445085 <n1445085@yandex.ru>
2024-10-23 00:58:14 +0300

    initial commit

---
+++ src/main/java/pro/sky/kursovik2/Kursovik2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.kursovik2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovik2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kursovik2Application.class, args);
+	}
+
+}
